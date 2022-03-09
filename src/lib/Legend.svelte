d052a68d8f5ac141175591f2a0e5527f21418563
Tobin Bradley <tobin.bradley@gmail.com>
2022-03-09 11:13:03 -0500

    almost there

--- src/lib/Legend.svelte
+++ src/lib/Legend.svelte
@@ -1,9 +1,7 @@
 <script>
-  import { selectedConfig, selectedData, yearIdx, colors } from "../store/store"
+  import { selectedConfig, selectedData, breaks, minBreak, yearIdx, colors, calcCounty, calcCountyRaw, calcSelected, calcSelectedRaw, selectedNeighborhoods } from "../store/store"
+  import { formatNumber, shortNumber } from "./utils"
 
-  // TODO: selected value
-  // TODO: county value
-  // TODO: legend values
   // TODO: legend hover
 </script>
 
@@ -11,16 +9,59 @@
   class="bg-white inline-block absolute top-0 left-0 shadow-md"
   style="width: 260px;"
 >
-  <h2 class="border-b-2 border-stone-200 p-2">
+  <h2 class="border-b-2 border-stone-200 p-1 font-semibold">
     {$selectedConfig.title},
     {#if $selectedData}
       {$selectedData.years[$yearIdx]}
     {/if}
   </h2>
-  <div class="text-center text-sm text-stone-600 p-2">
+  <div class="text-center text-sm text-stone-600 p-1">
     {$selectedConfig.subtitle}
   </div>
-  <div>
+
+  {#if $selectedData}
+  <div class="flex columns-2">
+    {#if $selectedNeighborhoods.length > 0}
+    <div class="flex-grow text-center">
+      <div class="text-sm">SELECTED</div>
+      <div class="font-semibold">
+        {formatNumber($calcSelected, $selectedConfig.format || null)}
+      </div>
+      {#if $selectedConfig.label}
+      <div class="lowercase text-sm">{$selectedConfig.label}</div>
+      {/if}
+      {#if $selectedConfig.raw_label}
+      <div class="text-sm">or</div>
+      <div class="font-semibold">
+        {formatNumber($calcSelectedRaw)}
+      </div>
+      <div class="text-sm lowercase">
+        {$selectedConfig.raw_label}
+      </div>
+      {/if}
+    </div>    
+    {/if}
+    <div class="flex-grow text-center">
+      <div class="text-sm">COUNTY</div>
+      <div class="font-semibold">{formatNumber($calcCounty, $selectedConfig.format || null)}</div>
+      {#if $selectedConfig.label}
+      <div class="lowercase text-sm">{$selectedConfig.label}</div>
+      {/if}
+      {#if $selectedConfig.raw_label}
+      <div class="text-sm">or</div>
+      <div class="font-semibold">
+        {formatNumber($calcCountyRaw)}
+      </div>
+      <div class="text-sm lowercase">
+        {$selectedConfig.raw_label}
+      </div>
+      {/if}
+    </div>
+  </div>
+  {/if}
+
+  {#if $breaks}
+  <div class="pt-1">
     <svg
       class="block w-full h-11"
       xmlns="http://www.w3.org/2000/svg"
@@ -68,24 +109,45 @@
           style="fill: {$colors[4]}"
         />
         <text x="-19.5" y="864.3" class="legendText">
-          <tspan x="-19.5" y="864.3">N1</tspan>
+          <tspan x="-19.5" y="864.3">{formatNumber($minBreak, "short")}</tspan>
         </text>
         <text y="864.4" x="28.6" class="legendText">
-          <tspan y="864.4" x="28.6">N2</tspan>
+          <tspan y="864.4" x="28.6">{formatNumber($breaks[0], "short")}</tspan>
         </text>
         <text x="78.4" y="864.4" class="legendText">
-          <tspan x="78.4" y="864.4">n3</tspan>
+          <tspan x="78.4" y="864.4">{formatNumber($breaks[1], "short")}</tspan>
         </text>
         <text y="864.4" x="128" class="legendText">
-          <tspan y="864.4" x="128">n4</tspan>
+          <tspan y="864.4" x="128">{formatNumber($breaks[2], "short")}</tspan>
         </text>
         <text x="177.8" y="864.4" class="legendText">
-          <tspan x="177.8" y="864.4">n5</tspan>
+          <tspan x="177.8" y="864.4">{formatNumber($breaks[3], "short")}</tspan>
         </text>
         <text y="864.3" x="225.8" class="legendText">
-          <tspan y="864.3" x="225.8">n6</tspan>
+          <tspan y="864.3" x="225.8">{formatNumber($breaks[4], "short")}</tspan>
         </text>
       </g>
     </svg>
   </div>
+  {/if}
 </div>
+
+
+<style>
+  .legendText {
+  font-family: Roboto,sans-serif;
+  font-size: 10px;
+  letter-spacing: 0;
+  line-height: 100%;
+  stroke-width: 1px;
+  text-align: center;
+  text-anchor: middle;
+  word-spacing: 0;
+}
+.legendText:last-of-type {
+  text-anchor: end;
+}
+.legendText:first-of-type {
+  text-anchor: start;
+}
+</style>
