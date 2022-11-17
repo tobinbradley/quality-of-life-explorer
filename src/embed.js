265dae7921c4f0228471db3b92fc63ab8f812779
tobinbradley <tobin.bradley@gmail.com>
2022-11-17 15:53:52 -0500

    add flyto, noninteractive for embed map

--- src/embed.js
+++ src/embed.js
@@ -2,12 +2,22 @@ import './tailwind.css'
 import './lib/registerServiceWorker'
 import Map from './lib/Map.svelte'
 
+// get params
+// flyto [btoa encoding of maplibre flyto params, null] default null
 let params = new URLSearchParams(document.location.search)
-const interactive = params.get("interactive")
+let flyto = null
+
+try {
+  flyto = window.atob(params.get("flyto"))
+  flyto = JSON.parse(flyto)
+} catch (error) {
+  flyto = null
+}
 
 new Map({
   target: document.getElementById("mapContainer"),
   props: {
-    interactive: interactive === 'false' ? false : true
+    flyto: flyto,
+    interactive: false
   }
 })
